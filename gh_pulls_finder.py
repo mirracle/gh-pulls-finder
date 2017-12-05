704ceb7d16ee7a3481e44e3ff01ea0b0f64f0a5d
Aaliev Taalai <jyldyz@MacBook-Air-Jyldyz.local>
2017-12-05 07:41:51 +0600

    Add Script

--- gh_pulls_finder.py
+++ gh_pulls_finder.py
@@ -2,9 +2,9 @@ from urllib.request import Request, urlopen
 import json
 import webbrowser
 
-def get_user_repos_url():
+def get_user_repos_url(token):
 	url = "https://api.github.com/user"
-	token = "112a65ea1b16d630621e81c50e58d1a8f24ba53a"
+	token = token
 	request = Request(url)
 	request.add_header('Authorization', 'token %s' % token)
 	response = urlopen(request)
@@ -13,9 +13,8 @@ def get_user_repos_url():
 	b = n.get("repos_url")
 	return b
 
-def get_user(repos_url):
+def get_user(repos_url, token):
 	url = repos_url
-	token = "112a65ea1b16d630621e81c50e58d1a8f24ba53a"
 	request = Request(url)
 	request.add_header('Authorization', 'token %s' % token)
 	response = urlopen(request)
@@ -27,11 +26,10 @@ def get_user(repos_url):
 		v.append(dictionary.get("url"))
 	return v
 
-def get_all_pulls_url(pulls_url):
+def get_all_pulls_url(pulls_url, token):
 	z = []
 	for items in range(len(pulls_url)):
 		url = pulls_url[items]
-		token = "112a65ea1b16d630621e81c50e58d1a8f24ba53a"
 		request = Request(url)
 		request.add_header('Authorization', 'token %s' % token)
 		response = urlopen(request)
@@ -42,11 +40,10 @@ def get_all_pulls_url(pulls_url):
 		z.append(e)
 	return z
 
-def get_open_pulls_url(pull_url):
+def get_open_pulls_url(pull_url, token):
 	z=[]
 	for items in pull_url:
 		url = items
-		token = "112a65ea1b16d630621e81c50e58d1a8f24ba53a"
 		request = Request(url)
 		request.add_header('Authorization', 'token %s' % token)
 		response = urlopen(request)
@@ -64,5 +61,6 @@ def open_link(links):
 	for items in links:
 		webbrowser.open(items)
 
-all_links = get_open_pulls_url(get_all_pulls_url(get_user(get_user_repos_url())))
+token = input("Please enter you access token: ")
+all_links = get_open_pulls_url(get_all_pulls_url(get_user(get_user_repos_url(token),token),token),token)
 open_link(all_links)
